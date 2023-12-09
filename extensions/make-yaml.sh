768c180281fd791fa7eb789c0d42662e3ee8ec91
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2023-12-09 15:59:13 +0100

    feat: first version

--- extensions/make-yaml.sh
+++ extensions/make-yaml.sh
@@ -2,23 +2,40 @@
 
 set -e
 
+mkdir -p extensions/yaml
+declare -A repos
+
 while IFS=, read -r category repo; do
-  # target="extensions/${category}/${repo}"
-  # if [ ! -d "${target}" ]; then
-  #   git submodule add "https://github.com/${repo}" "${target}"
-  # fi
+  repos["$repo"]=1
   meta="extensions/yaml/${repo//\//--}.yml"
-  # if [ ! -f "${meta}" ]; then
+  if [[ ! -f "${meta}" || (-f "${meta}" && $(find "$meta" -mtime +30)) ]]; then
     yaml_name="- name: $(basename ${repo})"
     yaml_path="path: https://github.com/${repo}"
     repoowner=$(gh repo view --json owner --jq ".owner.login" "${repo}")
-    author=$(gh api "https://api.github.com/users/${repoowner}" --jq ".name")
+    author=$(gh api "users/${repoowner}" --jq ".name")
+    if [[ -z "${author}" ]]; then
+      author=$(gh api "users/${repoowner}" --jq ".login")
+    fi
     yaml_author="author: \"[${author}](https://github.com/${repoowner}/)\""
     description=$(gh repo view --json description --jq ".description" "${repo}")
-    yaml_usage="  \n    \`\`\`sh\n    quarto add ${repo}\n    \`\`\`"
+    if [[ -z "${description}" ]]; then
+      description="No description available"
+    fi
+    description=$(echo "${description}" | sed 's/^[[:space:]]*//')
+    topics=$(gh api "repos/${repo}/topics" --jq ".names | map(select(. | contains(\"quarto\") | not))")
+    yaml_usage="\n    \n    \`\`\`sh\n    quarto add ${repo}\n    \`\`\`"
     yaml_description="description: |\n    ${description}${yaml_usage}"
-    yaml_categories="categories: [${category}]"
-    echo -e "${yaml_name}\n  ${yaml_path}\n  ${yaml_author}\n  ${yaml_description}\n  ${yaml_categories}\n" > "${meta}"
-  # fi
-
+    yaml_type="type: [${category}]"
+    yaml_categories="categories: ${topics}"
+    echo -e "${yaml_name}\n  ${yaml_path}\n  ${yaml_author}\n  ${yaml_description}\n  ${yaml_type}\n  ${yaml_categories}\n" > "${meta}"
+  fi
 done < extensions/quarto-extensions.csv
+
+for yaml in extensions/yaml/*.yml; do
+  repo_yaml=$(basename "${yaml}" .yml)
+  repo_yaml=${repo_yaml//--/\/}
+  if [[ -z ${repos["${repo_yaml%.*}"]} ]]; then
+    echo "Removing ${yaml} as its repo is not or no longer listed in the CSV file."
+    rm "${yaml}"
+  fi
+done
