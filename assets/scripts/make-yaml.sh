722c304cda3bb9a745a8a4481ee9a520b6904f73
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-03-22 15:44:08 +0100

    refctor: use a single source of truth as JSON (#148)

--- assets/scripts/make-yaml.sh
+++ assets/scripts/make-yaml.sh
@@ -5,94 +5,76 @@ set -e
 mkdir -p extensions/yaml
 mkdir -p authors
 
-# Dictionary to store the repositories
-# declare -A repos
-# repos["$repo"]=1
-
 author_listing_ref="assets/quarto/_author-listing.qmd"
 previous_repo_owner=""
 
-sort -f extensions/quarto-extensions.csv | while IFS=, read -r entry; do
-  repo=$(echo "${entry}" | cut -d'/' -f1,2)
-  author_listing="authors/${repo%%/*}.qmd"
-  meta="extensions/yaml/${repo//\//--}.yml"
-  if [[ ! -f "${meta}" || (-f "${meta}" && $(find "$meta" -mtime +30)) ]]; then
-    repo_info=$(gh repo view --json owner,description,createdAt,updatedAt,latestRelease,licenseInfo,stargazerCount,repositoryTopics "${repo}")
-    repo_created=$(echo "${repo_info}" | jq -r ".createdAt")
-    repo_release=$(echo "${repo_info}" | jq -r ".latestRelease.tagName")
-    if [[ "${repo_release}" = "null" ]]; then
-      repo_release="none"
-      repo_updated=$(echo "${repo_info}" | jq -r ".updatedAt")
-      yaml_usage_body="${entry}"
-    else
-      repo_release_url=$(echo "${repo_info}" | jq -r ".latestRelease.url")
-      yaml_usage_body="${entry}@${repo_release}"
-      repo_release="[${repo_release#v}]($repo_release_url)"
-      repo_updated=$(echo "${repo_info}" | jq -r ".latestRelease.publishedAt")
-    fi
-    repo_license=$(echo "${repo_info}" | jq -r ".licenseInfo.name")
-    if [[ "${repo_license}" = "null" ]]; then
-      repo_license="No license specified"
-    fi
-    repo_stars=$(echo "${repo_info}" | jq -r ".stargazerCount")
-    if [[ "${repo_stars}" = "null" ]]; then
-      repo_stars="0"
-    fi
-    repo_owner=$(echo "${repo_info}" | jq -r ".owner.login")
-    repo_author=$(gh api "users/${repo_owner}" --jq ".name")
-    if [[ -z "${repo_author}" ]]; then
-      repo_author=$(gh api "users/${repo_owner}" --jq ".login")
-    fi
-    repo_description=$(echo "${repo_info}" | jq -r ".description")
-    if [[ -z "${repo_description}" ]]; then
-      repo_description="No description available"
-    fi
-    repo_description=$(echo "${repo_description}" | sed 's/^[[:space:]]*//')
-    repo_description=$(echo "${repo_description}" | sed -E 's/([^`])(<[^<>]+>)([^`])/\1`\2`\3/g')
-    repo_topics=$(echo "${repo_info}" | jq -r ".repositoryTopics")
-    if [[ "${repo_topics}" = "null" ]]; then
-      repo_topics="[]"
-    else
-      repo_topics=$(echo "${repo_topics}" | jq -r "map(.name)")
-      repo_topics=$(echo "${repo_topics}" | jq -r 'map(select(.) | sub("^quarto-"; ""))')
-      repo_topics=$(echo "${repo_topics}" | jq -r 'map(select(.) | sub("-template[s]*"; ""))')
-      repo_topics=$(echo "${repo_topics}" | jq -r 'map(select(.) | if test("filters$|formats$|journals$") then sub("s$"; "") else . end)')
-      repo_topics=$(echo "${repo_topics}" | jq -r 'map(select(.) | sub("reveal-js"; "reveal.js") | sub("revealjs"; "reveal.js"))')
-      repo_topics=$(echo "${repo_topics}" | jq -r 'map(select(. | test("quarto|extension|^pub$") | not))')
-      repo_topics=$(echo "${repo_topics}" | jq -c 'unique')
-    fi
-    extension_title=$(basename "${repo}")
-    echo -e \
-      "- title: ${extension_title#quarto-}\n" \
-      " github-url: https://github.com/${repo}\n" \
-      " author: \"${repo_author}\"\n" \
-      " author-url: \"/${author_listing}\"\n" \
-      " date: \"${repo_created}\"\n" \
-      " file-modified: \"${repo_updated}\"\n" \
-      " categories: ${repo_topics}\n" \
-      " license: \"${repo_license}\"\n" \
-      " stars: \"$(printf "%05d\n" ${repo_stars})\"\n" \
-      " version: \"${repo_release}\"\n" \
-      " description: |\n    ${repo_description}\n" \
-      " usage: ${yaml_usage_body}\n" \
-      > "${meta}"
-    if [[ "${repo_owner}" == "${previous_repo_owner}" ]]; then
-      count_extensions=$((count_extensions+1))
-      count_stars=$((count_stars+repo_stars))
-    else
-      previous_repo_owner=${repo_owner}
-      count_extensions=1
-      count_stars=${repo_stars}
-    fi
-    sed \
-      -e "s/<<github-username>>/${repo%%/*}/g" \
-      -e "s/<<github-stars>>/${count_stars}/g" \
-      -e "s/<<github-stars-string>>/$(printf "%05d\n" ${count_stars})/g" \
-      -e "s/<<extensions-count>>/${count_extensions}/g" \
-      -e "s:<<github-repo>>:${repo}:g" \
-      -e "s/<<github-name>>/${repo_author}/g" \
-      "${author_listing_ref}" > "${author_listing}"
+json_file="quarto-extensions.json"
+git fetch origin quarto-wizard:quarto-wizard
+git restore --source quarto-wizard -- "${json_file}"
+
+jq -c 'to_entries[]' "${json_file}" | while read -r entry; do
+  entry_repo=$(echo "${entry}" | jq -r '.key')
+  meta="extensions/yaml/${entry_repo//\//--}.yml"
+
+  entry_owner=$(echo "${entry}" | jq -r '.value.owner')
+  author_listing="authors/${entry_owner}.qmd"
+
+  entry_title=$(echo "${entry}" | jq -r '.value.title')
+  entry_created=$(echo "${entry}" | jq -r '.value.createdAt')
+  entry_updated=$(echo "${entry}" | jq -r '.value.updatedAt')
+  entry_url=$(echo "${entry}" | jq -r '.value.url')
+  entry_topics=$(echo "${entry}" | jq -r '.value.repositoryTopics' | jq -c 'unique')
+  entry_license=$(echo "${entry}" | jq -r '.value.licenseInfo')
+  entry_stars=$(echo "${entry}" | jq -r '.value.stargazerCount')
+  entry_image=$(echo "${entry}" | jq -r '.value.openGraphImageUrl')
+  entry_author=$(echo "${entry}" | jq -r '.value.author')
+
+  entry_description=$(echo "${entry}" | jq -r '.value.description')
+  entry_description=$(echo "${entry_description}" | sed 's/^[[:space:]]*//')
+  entry_description=$(echo "${entry_description}" | sed -E 's/([^`])(<[^<>]+>)([^`])/\1`\2`\3/g')
+
+  entry_release=$(echo "${entry}" | jq -r '.value.latestRelease')
+  yaml_usage_body="${entry_repo}"
+  if [[ "${entry_release}" != "none" ]]; then
+    entry_release_url=$(echo "${entry}" | jq -r '.value.latestReleaseUrl')
+    yaml_usage_body="${entry_repo}@${entry_release}"
+    entry_release="[${entry_release#v}](${entry_release_url})"
   fi
+
+  echo -e \
+    "- title: ${entry_title}\n" \
+    " image: ${entry_image}\n" \
+    " image-alt: \"GitHub repository OpenGraph image for ${entry_url}\"\n" \
+    " github-url: ${entry_url}\n" \
+    " author: \"${entry_author}\"\n" \
+    " author-url: \"/${author_listing}\"\n" \
+    " date: \"${entry_created}\"\n" \
+    " file-modified: \"${entry_updated}\"\n" \
+    " categories: ${entry_topics}\n" \
+    " license: \"${entry_license}\"\n" \
+    " stars: $(printf "%05d\n" ${entry_stars})\n" \
+    " version: \"${entry_release}\"\n" \
+    " description: |\n    ${entry_description}\n" \
+    " usage: ${yaml_usage_body}\n" \
+    > "${meta}"
+
+  if [[ "${entry_owner}" == "${previous_entry_owner}" ]]; then
+    count_extensions=$((count_extensions+1))
+    count_stars=$((count_stars+entry_stars))
+  else
+    previous_entry_owner=${entry_owner}
+    count_extensions=1
+    count_stars=${entry_stars}
+  fi
+
+  sed \
+    -e "s/<<github-username>>/${entry_owner}/g" \
+    -e "s/<<github-stars>>/${count_stars}/g" \
+    -e "s/<<github-stars-string>>/$(printf "%05d\n" ${count_stars})/g" \
+    -e "s/<<extensions-count>>/${count_extensions}/g" \
+    -e "s:<<github-repo>>:${entry_repo}:g" \
+    -e "s/<<github-name>>/${entry_author}/g" \
+    "${author_listing_ref}" > "${author_listing}"
 done
 
 echo -e "extensions: $(wc -l < extensions/quarto-extensions.csv | tr -d ' ')\nauthors: $(ls -1 authors | wc -l | tr -d ' ')" > _variables.yml
